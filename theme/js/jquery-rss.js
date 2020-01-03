a9c9457d832457cc03e7f9d9bc79d9e097c7866a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-03 01:59:02 +0000

    Deploy JackMcKew/cyberlytica-website to github.com/JackMcKew/cyberlytica-website.git:gh-pages

---
+++ theme/js/jquery-rss.js
@@ -0,0 +1,10 @@
+$.get("https://jackmckew.dev/feeds/all.atom.xml", function (data) {
+    $(data).find("entry").each(function () { // or "item" or whatever suits your feed
+        var el = $(this);
+
+        console.log("------------------------");
+        console.log("title      : " + el.find("title").text());
+        console.log("author     : " + el.find("author").text());
+        console.log("description: " + el.find("description").text());
+    });
+});
